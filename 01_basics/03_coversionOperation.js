cd2a464e9d9ae5928e934b0cd3fbfab9a51f0d7e
Muhammad Yasir <yasirtahir009@gmail.com>
2025-02-02 01:55:54 +0500

    conversion Operation

---
+++ 01_basics/03_coversionOperation.js
@@ -0,0 +1,29 @@
+let score = "Yasir";
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// Discription:
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0;
+
+let isLoggedIn = "Yasir"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false;
+// "" => false;
+// "Yasir" => true
+
+
+let someNumber = 33;
+
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
