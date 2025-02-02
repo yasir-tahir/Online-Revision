2d7fd99aff97b2141d52b03a9dc4b8a38337d987
Muhammad Yasir <yasirtahir009@gmail.com>
2025-02-03 00:46:05 +0500

    operation comparison

--- 01_basics/03_coversionOperation.js
+++ 01_basics/03_coversionOperation.js
@@ -25,5 +25,39 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 let someNumber = 33;
 
 let stringNumber = String(someNumber);
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+// ********* Operations *********\\
+
+let value = 3;
+let negValue = -value;
+
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2%2);
+// console.log(2**2);
+
+let str1 = "Hello";
+let str2 = " Yasir";
+
+// console.log(str1 + str2);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log((3 + 4) * 5 % 3);
+
+// console.log(true);
+// console.log(+false);
+// console.log(+ 0);
+
+
+
---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,23 @@
+// console.log(2 > 1); //greater then
+// console.log(2 >= 1); //greater & equal
+// console.log(2 < 1); //less then
+// console.log(2 == 1); //equal
+// console.log(2 != 1); //not equal
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+console.log("2" === 2);
+console.log("2" == 2);
+
+
+
+
