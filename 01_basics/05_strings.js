a53f862ec98e3057217fe540ef9787c11020a4fc
Muhammad Yasir <yasirtahir009@gmail.com>
2025-02-08 02:06:32 +0500

    strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,38 @@
+const name = "yasir";
+const repoCount = 30;
+
+// console.log(name + repoCount + " Value"); //Old method
+// console.log(`Hello my name is ${name} and my respo count
+//     is ${repoCount}`); //new method
+
+// const gameName = new String('yasir');
+// console.log(gameName[3]);
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(3));
+// console.log(gameName.indexOf('s'));
+
+// const gameName = new String('yasir-ir');
+
+// const newString = gameName.substring(0, 2)
+// console.log(newString);
+
+// const newString = gameName.slice(-6, 4)
+// console.log(newString);
+
+
+// const newString = "     yasir   ";
+// console.log(newString);
+// // console.log(newString.trim());
+
+// const url = "https://yasir.com/yasir%20tahir"
+
+// console.log(url.replace('%20', '@'));
+// console.log(url.includes('yasir'));
+
+const gameName = 'yasir-saad-yasir'
+console.log(gameName.split('-'));
+
+
