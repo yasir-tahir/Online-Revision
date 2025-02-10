155885fd36d373733d4739bb3cee40469c002734
Muhammad Yasir <yasirtahir009@gmail.com>
2025-02-11 01:40:24 +0500

    numbers

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,26 @@
+// const score  = 400;
+// const balance = new Number(100); //ye number define kar raha hai
+// const balance1 = new String(100); //ye string define kar raha hai
+// const balance2 = new Boolean(100); //ye boolean define kar raha hai
+
+
+// console.log(score);
+// console.log(balance);
+// console.log(balance1);
+// console.log(balance2);
+
+// console.log(balance.toString()); // ye value string mai convert karega
+// console.log(balance.toString().length); // ye string ki length bataega 
+// console.log(balance.toFixed(2)); // ye amount ke agge units fixed karta hai 
+
+// const otherNumber = 23.899 // 0.00 points kam karne ke kam bhi atta hai 
+// console.log(otherNumber.toPrecision(2)); // ye method amount decrease karta hai
+
+// const hundreds = 1000000
+// console.log(hundreds.toLocaleString()); // Us standard value (Like 1,000,000);
+// console.log(hundreds.toLocaleString('en-IN')); // Indian standard value (Like 10,00,000)
+
+
+ 
+
+
