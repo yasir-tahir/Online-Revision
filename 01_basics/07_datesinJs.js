41ef2873321e73f8111ce7a7ee8517736f8fa776
Muhammad Yasir <yasirtahir009@gmail.com>
2025-02-13 01:38:10 +0500

    dates

---
+++ 01_basics/07_datesinJs.js
@@ -0,0 +1,19 @@
+//Dates
+
+let myDate = new Date();
+
+// console.log(myDate.toDateString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toJSON());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toLocaleTimeString());
+// console.log(myDate.toString());
+// console.log(myDate.toUTCString());
+
+
+// let myCreatedDate = new Date (2025, 1, 13 )
+// console.log(myCreatedDate.toDateString());
+
+let myCreatedDate = new Date (2025, 1, 13, 5, 33 )
+console.log(myCreatedDate.toLocaleString());
