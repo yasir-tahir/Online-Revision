1a31f982c29233b791565587edbf243e5766ca40
Muhammad Yasir <yasirtahir009@gmail.com>
2025-02-13 01:16:28 +0500

    math and nums

--- 01_basics/06_nums_and_math.js
+++ 01_basics/06_nums_and_math.js
@@ -1,3 +1,6 @@
+
+// ********* Numbers Details ********** \\
+
 // const score  = 400;
 // const balance = new Number(100); //ye number define kar raha hai
 // const balance1 = new String(100); //ye string define kar raha hai
@@ -21,6 +24,25 @@
 // console.log(hundreds.toLocaleString('en-IN')); // Indian standard value (Like 10,00,000)
 
 
+// ********* Math Details ********** \\
+
+//  console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+// console.log(Math.round(4.4));
+// console.log(Math.ceil(4.4));
+// console.log(Math.floor(4.4));
+// console.log(Math.min(4, 3, 1, 0));
+// console.log(Math.max(4, 3, 1, 0));
+
  
+console.log(Math.random());
+console.log((Math.random()*10) + 1);
+console.log(Math.floor((Math.random()*10)) + 1);
+
+const min = 10;
+const max = 20;
+
+console.log(Math.floor(Math.random()* (max - min + 1)) + min );
 
 
