8c59179b12b6b7d7b612debcb7117256902e0091
Muhammad Yasir <yasirtahir009@gmail.com>
2025-04-14 00:48:26 +0500

    object target & source

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,54 @@
+// const tinderUser = new Object() //singleton Object
+// const tinderUsers = {} // nonsingleton Object
+
+// console.log(tinderUser);
+// console.log(tinderUsers);
+
+// ------------------
+
+// const tinderUser = {}
+
+// tinderUser.id = "abc123"
+// tinderUser.name = "Yasir"
+// tinderUser.isLoggedIn = false
+
+// console.log(tinderUser);
+
+// -----------------------
+
+
+// const regularUser = {
+//     email: "yasir@gmail.com",
+//     fullname: {
+//         userfullname: {
+//             firstname: "Yasir",
+//             lastname: "Tahir"
+//         }
+//     }
+// }
+
+// console.log(regularUser.fullname.userfullname.firstname);
+
+
+// -------------------------
+
+
+//  const obj1 = {1: 'a', 2: 'b'}
+//  const obj2 = {3: 'a', 4: 'b'}
+
+//  const obj3 = {obj1 , obj2}
+
+//  console.log(obj3);
+ 
+
+//  const obj1 = {1: 'a', 2: 'b'}
+//  const obj2 = {3: 'a', 4: 'b'}
+
+//  const obj3 = Object.assign(obj1, obj2)
+//  const obj3a = Object.assign({}, obj1, obj2) //target 
+//  const obj3b = {...obj1, ...obj2} //sprade operator
+
+
+//  console.log(obj3);
+//  console.log(obj3a);
+//  console.log(obj3b);
