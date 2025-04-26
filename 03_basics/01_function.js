24a96a68b41fb1c4b980d57b5e46d35e7fdeacc7
Muhammad Yasir <yasirtahir009@gmail.com>
2025-04-27 01:54:37 +0500

    function practice

--- 03_basics/01_function.js
+++ 03_basics/01_function.js
@@ -75,6 +75,64 @@ function loginUserMessage(username = 'Yasir'){
         return `${username} just logged in`
     }
     
-    console.log(loginUserMessage("Saad"));
-    console.log(loginUserMessage());
+    // console.log(loginUserMessage("Saad"));
+    // console.log(loginUserMessage());
     
+
+    // ------------------------------------
+
+    // function calculateCartPrice(num1){
+    //     return num1
+    // }
+
+    // console.log(calculateCartPrice(200, 300, 400));
+    
+
+    // function calculateCartPrice(...num1){ //this is sprade/rest operator
+    //     return num1
+    // }
+
+    // console.log(calculateCartPrice(200, 300, 400));
+    
+
+    // function calculateCartPrice(val1, val2, ...num1){ //this is only rest operator
+    //     return num1
+    // }
+
+    // console.log(calculateCartPrice(200, 300, 400));
+    
+// 1st Method:
+    const user = {
+        name: "Yasir",
+        price: 1000
+    }
+
+    function handleObject(anyobject){
+        // console.log(`Username is ${anyobject.name} and price is ${anyobject.price}`);
+        
+    }
+
+    // handleObject(user)
+
+    // 2nt Method:
+
+
+    function handleObject(anyobject){
+        // console.log(`Username is ${anyobject.name} and price is ${anyobject.price}`);
+        
+    }
+
+    handleObject({
+        name:"Yasir",
+        price:1222
+    })
+
+    // ---------------
+
+    const myNewArray = [200, 400, 600, 800]
+
+    function returnSecondArray(getArray){
+        return getArray[1]
+    }
+
+console.log(    returnSecondArray(myNewArray));
